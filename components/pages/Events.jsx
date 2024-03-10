cb9a1da9d35a48acb65cb5238afb345c48ca8dce
vineethkv7736 <vineethkv7736@gmail.com>
2024-03-10 15:28:15 +0530

    Event sliding

--- components/pages/Events.jsx
+++ components/pages/Events.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import Heading from '../Heading'
+"use client"
+import React from "react";
+import Heading from "../Heading";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const Events = () => {
   return (
-    <section id='events' className='min-h-screen w-full px-4 md:px-10 lg:px-24'>
-      <Heading title={'Events'}/>
+    <section
+      id="events"
+      className="min-h-screen w-full px-4 md:px-10 lg:px-24 text-white"
+    >
+      <Heading title={"Events"} />
+      <div>
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            <CarouselItem>vine</CarouselItem>
+            <CarouselItem>12</CarouselItem>
+            <CarouselItem>7736</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
