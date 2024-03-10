55e18f17a024b9c8a2ef4be569ab3bb4a773c593
vineethkv7736 <vineethkv7736@gmail.com>
2024-03-10 14:13:06 +0530

    About added

--- components/pages/About.jsx
+++ components/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Heading from "../Heading";
+import Counter from "../Counter";
 
 const About = () => {
   return (
     <section id="about" className="min-h-screen w-full px-24">
       <Heading title={'About'}/>
+      <div>
+        <Counter/>
+      </div>
     </section>
   );
 };
