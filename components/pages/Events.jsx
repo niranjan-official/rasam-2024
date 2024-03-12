7a38ca8d583c5c50dae913de5f97082d9623f906
vineethkv7736 <vineethkv7736@gmail.com>
2024-03-12 19:54:44 +0530

    Events edited

--- components/pages/Events.jsx
+++ components/pages/Events.jsx
@@ -1,17 +1,17 @@
-"use client";
 import React from "react";
 import Heading from "../Heading";
-import CarouselBody from "../CarouselBody";
+import ReactCarousel from "../ReactCarousel";
 
 const Events = ({ images }) => {
   return (
     <section
       id="events"
-      className="h-max md:min-h-screen w-full px-4 md:px-10 lg:px-24 text-white"
+      className="h-max w-full px-4 md:px-10 lg:px-24 text-white"
     >
       <Heading title={"Events"} />
-      <div className="flex justify-center items-center py-8">
-      <CarouselBody images={images}/>
+      <div className="w-full flex justify-center items-center py-8">
+        <ReactCarousel
+          images={images} />
       </div>
     </section>
   );
